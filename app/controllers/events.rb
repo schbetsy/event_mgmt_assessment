4664959ca6a8204f816757bdab3130033789a95a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-03 10:51:52 -0500

    CRUD complete

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -1,11 +1,15 @@
 #======== GET ==========================
 
 get "/event/view/:event" do
-  @event = Event.find(params[:event])
-  if @event.creator == current_user
-    erb :view_event
+  @event = Event.find_by_id(params[:event])
+  if @event == nil
+    erb :DNE
   else
-    erb :view_event
+    if @event.creator == current_user
+      erb :edit_event
+    else
+      erb :view_event
+    end
   end
 end
 
@@ -29,10 +33,25 @@ end
 post "/event/destroy" do 
   event = Event.find(params[:event])
   if event.creator == current_user
-    name = event.name
     event.destroy
     redirect to '/user/home?del=true'
   else
     redirect to '/user/home?fail=true'
+  end
+end
+
+post "/event/update" do
+  @event = Event.find(params[:id])
+  if @event.creator == current_user
+    @event.update_attributes(params[:event])
+    @error = @event.errors.full_messages
+    if @error.any?
+      @error = @error.join(", ")
+      erb :edit_event
+    else
+      redirect to "/event/view/#{@event.id}"
+    end
+  else
+    # Error message: "you can't update that record"
   end
 end

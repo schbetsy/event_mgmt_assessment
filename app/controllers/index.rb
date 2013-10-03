97b78f619dc422485c4b4957e1b63fc5a60812e2
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-03 09:08:05 -0500

    user handling and validations

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,35 @@
+#======== GET ==========================
 get '/' do
-  # Look in app/views/index.erb
+  @error_login = "username or password incorrect" if params[:error_login]
+  @error_signup = "something went wrong" if params[:error_signup]
   erb :index
+end
+
+get '/log_out' do
+  session.clear
+  redirect to '/'
+end
+
+#======== POST ===========================
+
+post '/sign_in' do 
+  user = User.authenticate(params[:user])
+  if user
+    session[:user_id] = user.id
+    erb :user_home
+  else
+    redirect to '/?error_login=true' 
+  end
+
+end
+
+post '/sign_up' do 
+  user = User.create(params[:user])
+  @error_signup = user.errors.full_messages
+  if @error_signup.any?
+    @error_signup = @error_signup.join(", ")
+    erb :index
+  else
+    erb :user_home
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ActiveRecord::Base
+  has_many :attended_events
+  has_many :created_events
+
   validates :password, presence: true
   validates :email, presence: true, uniqueness: true, 
-              format: { with: /\A.+@.+\..+\z/
+              format: { with: /\A.+@.+\..+\z/,
                         message: "must be a valid email"}
+
+  include BCrypt
+
+  def password=(new_password)
+    return nil if new_password == ""
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def password
+    return nil unless password_digest
+    @password ||= Password.new(password_digest)
+  end
+
+  def self.authenticate(args)
+    user = User.find_by_email(args[:email])
+    return user if user && (user.password == args[:password])
+    nil
+  end
+
+
 end

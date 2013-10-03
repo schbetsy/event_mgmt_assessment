f83654946ccb9d53a36ae26e9bd10a8bd8fde543
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-03 08:36:41 -0500

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true, 
+              format: { with: /\A.+@.+\..+\z/
+                        message: "must be a valid email"}
 end
